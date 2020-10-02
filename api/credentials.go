59a5184468bad5e0ccb43bc4e829e446b74f6a65
Ari Rigan <ari.rigan@gmail.com>
2020-10-01 23:41:30 -0700

    remove email

--- api/credentials.go
+++ api/credentials.go
@@ -3,6 +3,5 @@ package api
 //Credentials respresents the user login object
 type Credentials struct {
 	Username string `json:"username"`
-	Email    string `json:"email"`
 	Password string `json:"password"`
 }
